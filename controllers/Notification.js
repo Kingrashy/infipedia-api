59065a29e258b45e9b421b84ae34afe9a2d9352a
MoonswapFinance <ekwuemerapheal152@gmail.com>
2023-06-29 03:49:13 +0100

    updated

--- controllers/Notification.js
+++ controllers/Notification.js
@@ -3,10 +3,17 @@ import NotificationModel from "../models/Notification.js";
 export const getUserNotification = async (req, res) => {
   const { userId } = req.params;
   try {
-    const notification = await NotificationModel.find({ userId: userId }).sort({
-      createdAt: -1,
-    });
+    const notification = await NotificationModel.find({ userId: userId });
     res.status(200).json(notification);
+  } catch (error) {
+    console.log({ error: error.message });
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const ReadNotification = async (req, res) => {
+  try {
+    const { userId } = req.params;
   } catch (error) {
     console.log({ error: error.message });
     res.status(500).json({ error: error.message });
